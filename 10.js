f4316de4076755a01954e4cb576b76966e4d4a5e
方舟 <576156845@qq.com>
2021-08-02 11:55:12 +0800

    面试更新

---
+++ 10.js
@@ -0,0 +1,16 @@
+import anime from 'animejs'
+var bouncingBall = anime({
+	targets: '.ball',
+	translateY: '50vh',
+	duration: 300,
+	loop: true,
+	direction: 'alternate',
+	easing: 'easeInCubic',
+	scaleX: {
+		value: 1.05,
+		duration: 150,
+		delay: 268
+	}
+});
+
+bouncingBall()
